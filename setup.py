141139acb94061b9c07068903bfaa4e935eeea8c
Ed Lucas <elucas@whiteoctober.co.uk>
2017-08-18 14:39:22 +0100

    Renamed library

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
 setup(
-    name='okta-minimal',
+    name='okta_minimal',
     version='0.1',
     description='Just enough Okta logic to do web logins',
-    url='https://github.com/whiteoctober/okta-minimal',
+    url='https://github.com/whiteoctober/okta_minimal',
     author='Ed Lucas',
     author_email='elucas@whiteoctober.co.uk',
     license='MIT',
-    packages=['okta-minimal']
+    packages=['okta_minimal']
 )
