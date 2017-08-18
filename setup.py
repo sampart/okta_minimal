7fdd08f554cfe7448e4e81ad59980b24c4d08504
Ed Lucas <elucas@whiteoctober.co.uk>
2017-08-18 15:07:55 +0100

    Forcing version change

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='okta_minimal',
-    version='0.1',
+    version='0.2',
     description='Just enough Okta logic to do web logins',
     url='https://github.com/whiteoctober/okta_minimal',
     author='Ed Lucas',
