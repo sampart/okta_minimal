2209f44667e6cdd2b1bb82d0530ef0b9edda1d16
Ed Lucas <elucas@whiteoctober.co.uk>
2017-08-18 13:57:26 +0100

    Added setup for pip installer

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='okta-minimal',
+    version='0.1',
+    description='Just enough Okta logic to do web logins',
+    url='https://github.com/whiteoctober/okta-minimal',
+    author='Ed Lucas',
+    author_email='elucas@whiteoctober.co.uk',
+    license='MIT',
+    packages=['okta-minimal']
+)
